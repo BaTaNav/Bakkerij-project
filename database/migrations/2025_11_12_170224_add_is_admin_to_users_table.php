7ae6a48e9650533385465845a8369a30b9a4858e
BaTaNav <77064177+BaTaNav@users.noreply.github.com>
2025-11-12 18:03:49 +0100

    Add 'is_admin' column to users table

---
+++ database/migrations/2025_11_12_170224_add_is_admin_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+  public function up(): void
+{
+    Schema::table('users', function (Blueprint $table) {
+        // Voegt een 'true/false' kolom toe (een boolean)
+        // 'default(false)' zorgt dat elke nieuwe gebruiker automatisch GEEN admin is.
+        $table->boolean('is_admin')->default(false)->after('email');
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
